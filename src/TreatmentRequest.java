6284825c189e4fcacaae5882fb2b39a08b1052b1
fratyaldiz <firatylz2004@hotmail.com>
2025-12-18 09:17:42 +0300

    Initial commit

--- src/TreatmentRequest.java
+++ src/TreatmentRequest.java
@@ -1,4 +1,9 @@
 public class TreatmentRequest {
-    private int patientId;
-    private long arrivalTime;
+    int patientId;
+    long arrivalTime;
+
+    public TreatmentRequest(int patientId) {
+        this.patientId = patientId;
+        this.arrivalTime = System.currentTimeMillis();
+    }
 }
