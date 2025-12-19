e7cccd44a702ddaceb913c16f1f8cfec816254d1
fratyaldiz <firatylz2004@hotmail.com>
2025-12-19 21:44:25 +0300

    Initial commit

---
+++ src/DischargeRecord.java
@@ -0,0 +1,13 @@
+public class DischargeRecord {
+    int patientId;
+    long dischargeTime;
+
+    public DischargeRecord(int patientId) {
+        this.patientId = patientId;
+        this.dischargeTime = System.currentTimeMillis();
+    }
+
+    public String toString() {
+        return "PatientID=" + patientId + " dischargeTime=" + dischargeTime;
+    }
+}
